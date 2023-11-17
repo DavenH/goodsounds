b44ddf3df5feaef2151228ca70f9dc953d9cd04b
Daven <daven.hughes@gmail.com>
2023-11-16 21:00:51 -0400

    More experimentation with the FSD 2019 dataset

--- dataset.py
+++ dataset.py
@@ -4,6 +4,7 @@ import time
 
 import pandas as pd
 import torch
+import torch.nn as nn
 import torchaudio
 from alive_progress import alive_bar
 from torch.utils.data import Dataset
@@ -11,19 +12,20 @@ from torchaudio.transforms import Resample, MelSpectrogram, AmplitudeToDB
 
 
 class BaseAudioDataset(Dataset):
-    def __init__(self, trunc_len, width, height):
+    def __init__(self, sample_rate, trunc_len, width, height):
         self.trunc_len = trunc_len
-        samp_rate = 16000
+        n_fft=height * 4
+
         self.resamplers = {
-            32000: Resample(orig_freq=32000, new_freq=samp_rate),
-            44100: Resample(orig_freq=44100, new_freq=samp_rate),
-            48000: Resample(orig_freq=48000, new_freq=samp_rate)
+            32000: Resample(orig_freq=32000, new_freq=sample_rate),
+            44100: Resample(orig_freq=44100, new_freq=sample_rate),
+            48000: Resample(orig_freq=48000, new_freq=sample_rate)
         }
 
         hop_length = trunc_len // width
         self.mel_spectrogram_transform = MelSpectrogram(
-            sample_rate=samp_rate,
-            n_fft=height * 4,
+            sample_rate=sample_rate,
+            n_fft=n_fft,
             hop_length=hop_length,
             n_mels=height + 1
         )
@@ -31,9 +33,19 @@ class BaseAudioDataset(Dataset):
         self.amplitude_to_db_transform = AmplitudeToDB()
         self.data_cache = dict()
 
+        self.config = dict(
+            sample_rate=sample_rate,
+            n_fft=n_fft,
+            n_mels=height,
+            hop_length=hop_length,
+            trunc_len=trunc_len
+        )
+
     def preprocess_audio(self, audio, sr):
         # Resample, convert to mono, trim or pad, and transform
-        audio = self.resamplers[int(sr)](audio)
+        if sr != self.config["sample_rate"]:
+            audio = self.resamplers[int(sr)](audio)
+
         if audio.shape[0] > 1:
             audio = audio[0, :].unsqueeze(0)
         if audio.shape[1] < self.trunc_len:
@@ -57,6 +69,7 @@ class BaseAudioDataset(Dataset):
                 self.data_cache[i] = data
                 time.sleep(0.001)
                 bar()
+
         end_time = time.time_ns()
         print(f"Done loading in {(end_time - start_time)/1e9:5.03f}s")
 
@@ -66,10 +79,17 @@ class BaseAudioDataset(Dataset):
     def __getitem__(self, idx):
         raise NotImplementedError
 
+    def get_loss_function(self):
+        raise NotImplementedError
 
-class GoodSoundsDataset(BaseAudioDataset):
-    def __init__(self, root_path: str, trunc_len: int, width: int, height: int):
-        super().__init__(trunc_len, width, height)
+    def get_config(self):
+        self.config["size"] = self.__len__()
+        return self.config
+
+
+class GoodSounds(BaseAudioDataset):
+    def __init__(self, root_path: str, sample_rate: int, trunc_len: int, width: int, height: int):
+        super().__init__(sample_rate, trunc_len, width, height)
 
         self.conn = sqlite3.connect(os.path.join(root_path, 'database.sqlite'))
         self.root_path = root_path
@@ -77,7 +97,7 @@ class GoodSoundsDataset(BaseAudioDataset):
 
         self.cursor.execute("SELECT DISTINCT instrument from sounds WHERE instrument != ''")
         self.categories = [inst[0] for inst in self.cursor.fetchall()]
-        self.map_to_index = dict([(category, i) for (i, category) in enumerate(self.categories)])
+        self.label_map = dict([(category, i) for (i, category) in enumerate(self.categories)])
 
         query = """SELECT s.id, s.instrument, s.semitone, t.filename
 FROM sounds s
@@ -90,7 +110,6 @@ JOIN takes t ON s.id = t.sound_id
         # 16308 items in the dataset; we can probably load it all into memory
         # 128 * 256 * 4 = 2GB
         # perhaps even video memory - we'd need to load it all upfront then transfer to cuda device
-        self.preload()
 
     def __len__(self):
         # Return the total number of sound samples
@@ -107,47 +126,75 @@ JOIN takes t ON s.id = t.sound_id
             mel_spec = self.extract_features(audio)
 
             return {
-                    'spectrogram': mel_spec,
-                    'index': self.map_to_index[instrument],
-                    'metadata': {
-                        'path': path,
-                        'label': instrument,
+                'spectrogram': mel_spec,
+                'label': self.label_map[instrument],
+                'metadata': {
+                    'path': path,
+                    'label': instrument,
 
-                        # we may want to predict this too
-                        'note': note
-                    }
+                    # we may want to predict this too
+                    'note': note
                 }
+            }
+
+    def get_loss_function(self):
+        return nn.CrossEntropyLoss()
 
 
-class FSDKaggle2019Dataset(BaseAudioDataset):
-    def __init__(self, csv_file, root_dir, trunc_len, width, height):
-        super().__init__(trunc_len, width, height)
-        self.annotations = pd.read_csv(csv_file)
-        self.root_dir = root_dir
-        self.label_map = {label: idx for idx, label in enumerate(self.annotations['labels'].unique())}
 
-        self.preload() # if you have the ram. TODO This needs to be bounded
+class FSDKaggle2019(BaseAudioDataset):
+    def __init__(self, annotation_csv, wav_dir, sample_rate, trunc_len, width, height):
+        super().__init__(sample_rate, trunc_len, width, height)
+
+        self.wav_dir = wav_dir
+        self.annotations = pd.read_csv(annotation_csv)
+
+        # Get unique labels
+        uniq_values = set()
+        self.annotations['labels'].str.split(',').apply(uniq_values.update)
+        self.categories = list(uniq_values)
+        self.categories.sort()
+        self.label_map = {label: idx for idx, label in enumerate(self.categories)}
 
     def __len__(self):
+        # return 500
         return len(self.annotations)
 
     def __getitem__(self, idx):
         if idx in self.data_cache:
             return self.data_cache[idx]
         else:
-            audio_file = os.path.join(self.root_dir, self.annotations.iloc[idx, 0])
+            audio_file = os.path.join(self.wav_dir, self.annotations.iloc[idx, 0])
             waveform, sample_rate = torchaudio.load(audio_file)
             waveform = self.preprocess_audio(waveform, sample_rate)
             mel_spec = self.extract_features(waveform)
 
             label = self.annotations.iloc[idx, 1]
-            label_index = self.label_map[label]
+            labels = label.split(',')
+            label_indices = [self.label_map[label] for label in labels]
+            binary_labels = torch.zeros(len(self.label_map))
+            binary_labels[label_indices] = 1
 
             return {
                 'spectrogram': mel_spec,
-                'index': label_index,
+                'label': binary_labels,
                 'metadata': {
                     'path': audio_file,
                     'label': label
                 }
             }
+
+    def get_loss_function(self):
+        return nn.BCEWithLogitsLoss()
+
+    def calc_accuracy(self, output, target):
+        # Apply threshold to output to get binary predictions
+        preds = output >= 0.0
+        # Convert to byte tensors for comparison
+        preds = preds.to(torch.uint8)
+        target = target.to(torch.uint8)
+        # Compare predictions with true labels
+        correct = (preds == target).all(dim=1)  # Correct if all labels match
+        # Calculate accuracy
+        accuracy = correct.sum().float() / target.size(0)
+        return accuracy
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
+import json
+import os.path
 import random
+from base64 import b64encode
 
 import plotly.graph_objects as go
 import torch
-import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from plotly.subplots import make_subplots
 from torch.utils.data import DataLoader
 
 from cnn import ConvNetModel
-from dataset import GoodSoundsDataset
+from dataset import GoodSounds, FSDKaggle2019
 
-# Apply the dataset class
-root_path = '/home/daven/data/audio/good-sounds'
-width, height = 256, 128
+width, height = 512, 128
 truncate_len = 65536
-batch_size = 128
-epochs = 20
-
+sample_rate = 16000
+batch_size = 512
+epochs = 200
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def save_checkpoint(state, filename):
@@ -32,28 +32,50 @@ def load_checkpoint(filename):
 if __name__ == "__main__":
     import sys
     print(sys.argv)
+
+    # first parameter is root directory of dataset
     root_path = sys.argv[1]
-    dataset = GoodSoundsDataset(root_path, truncate_len, width, height)
-    model = ConvNetModel(width, height, len(dataset.categories)).to(device)
+
+    # second parameter is name of dataset
+    ds_class_name = sys.argv[2]
+
+    if ds_class_name == "GoodSounds":
+        dataset = GoodSounds(root_path, sample_rate, truncate_len, width, height)
+        train_amt = round(0.8 * len(dataset))
+        train_set, eval_set = torch.utils.data.random_split(dataset, [train_amt, len(dataset) - train_amt])
+    elif ds_class_name == "FSDKaggle2019":
+        train_set = FSDKaggle2019(root_path + '/FSDKaggle2019.meta/train_curated_post_competition.csv',
+                                  root_path + '/FSDKaggle2019.audio_train_curated',
+                                  sample_rate, truncate_len, width, height)
+        eval_set = FSDKaggle2019(root_path + '/FSDKaggle2019.meta/test_post_competition.csv',
+                                  root_path + '/FSDKaggle2019.audio_test',
+                                  sample_rate, truncate_len, width, height)
+    else:
+        raise ModuleNotFoundError
+
+    # if these aren't the same due to our split, then we will have BIG errors
+    assert train_set.categories == eval_set.categories
+
+    model = ConvNetModel(width, height, len(train_set.categories)).to(device)
     optim = optim.Adam(model.parameters())
+    loss_fn = train_set.get_loss_function().to(device)
+    train_set.preload()
+    eval_set.preload()
 
     # when our loss curve plateaus, there's almost always more room for
     # improvement by reducing the learning rate.
-    scheduler = ReduceLROnPlateau(optim, mode='min', factor=0.7, patience=8, verbose=True)
-
-    loss_fn = nn.CrossEntropyLoss().to(device)
+    scheduler = ReduceLROnPlateau(optim, mode='min', factor=0.5, patience=2, verbose=True)
+    # scheduler = CosineAnnealingLR(optim, 20)
     torch.manual_seed(0)
-
-    train_set, eval_set = torch.utils.data.random_split(dataset, [15308, 1000])
 
     train_dl = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     eval_dl = DataLoader(eval_set, batch_size=batch_size, shuffle=True)
-    # writer = SummaryWriter()
 
     start_epoch = 0
     count = 0
     best_eval_loss = 1e9
-    # start_epoch, count = load_checkpoint('checkpoints/epoch_249_step_31748_loss_0.00013662.pth.tar')
+    score_stagnation = 0
+    # start_epoch, count = load_checkpoint("checkpoints/FSDKaggle2019/epoch_7_step_144_loss_0.056546.pth.tar")
 
     for epoch in range(start_epoch, epochs):
         # puts the dropout and batchnorm components into correct mode
@@ -64,7 +86,7 @@ if __name__ == "__main__":
             optim.zero_grad()
 
             inputs = batch['spectrogram'].to(device)
-            labels = batch['index'].to(device)
+            labels = batch['label'].to(device)
 
             # predict our outputs with the model
             outputs = model(inputs)
@@ -81,53 +103,75 @@ if __name__ == "__main__":
 
             count += 1
 
-            if count % 10 == 0:
-                print(f"Epoch: {epoch}, step {count}, loss: {loss.item():3.3}")
+            # if count % 10 == 0:
+            print(f"Epoch: {epoch}, step {count}, loss: {loss.item():3.3}")
+        # accum_accuracy = 0.0
 
-        print(f"Evaluating epoch {epoch}...")
+        if epoch % 5 == 0:
+            print(f"Evaluating epoch {epoch}...")
 
-        model.eval()
-        accum_eval_loss = 0.0
-        accum_accuracy = 0.0
+            model.eval()
+            accum_eval_loss = 0.0
+            # turning off the gradient calculations speeds up inference
+            with torch.no_grad():
+                for batch in eval_dl:
+                    inputs = batch['spectrogram'].to(device)
+                    labels = batch['label'].to(device)
+                    outputs = model(inputs)
 
-        # turning off the gradient calculations speeds up inference
-        with torch.no_grad():
-            for batch in eval_dl:
-                inputs = batch['spectrogram'].to(device)
-                labels = batch['index'].to(device)
-                outputs = model(inputs)
-                pred_idcs = outputs.argmax(dim=1)
+                    # pred_idcs = outputs.argmax(dim=1)
+                    #
+                    # # calculate categorization accuracy
+                    # _, predicted = torch.max(outputs.data, 1)
+                    # correct = (predicted == labels).sum().item()
+                    # accum_accuracy += correct / labels.size(0)
 
-                # calculate categorization accuracy
-                _, predicted = torch.max(outputs.data, 1)
-                correct = (predicted == labels).sum().item()
-                accum_accuracy += correct / labels.size(0)
+                    loss = loss_fn(outputs, labels)
+                    accum_eval_loss += loss.item()
 
-                loss = loss_fn(outputs, labels)
-                accum_eval_loss += loss.item()
+            avg_eval_loss = accum_eval_loss / len(eval_dl)
+            # avg_eval_acc = accum_accuracy / len(eval_dl)
+            scheduler.step(avg_eval_loss)
 
-        avg_eval_loss = accum_eval_loss / len(eval_dl)
-        avg_eval_acc = accum_accuracy / len(eval_dl)
-        scheduler.step(avg_eval_loss)
+            # print(f"Eval loss: {avg_eval_loss:.6}, eval accuracy: {100*avg_eval_acc:3.2f}%")
+            print(f"Eval loss: {avg_eval_loss:.6}")
 
-        print(f"Eval loss: {avg_eval_loss:.6}, eval accuracy: {100*avg_eval_acc:3.2f}%")
+            # checkpoint if it's an improvement
+            if avg_eval_loss < best_eval_loss * 0.995:
+                best_eval_loss = avg_eval_loss
 
-        # checkpoint if it's an improvement
-        if avg_eval_loss < best_eval_loss:
-            best_eval_loss = avg_eval_loss
-            filename = f"checkpoints/t65k/epoch_{epoch}_step_{count}_loss_{avg_eval_loss:.6f}.pth.tar"
-            print(f"Saving checkpoint {filename}...")
+                config = dict(model=model.get_config(), dataset=train_set.get_config(), batch_size=batch_size)
+                hash_str = f"{hash(json.dumps(config, sort_keys=True))}"
+                config_hash = str(b64encode(hash_str.encode('utf-8')))[4:10]
+                folder = f"checkpoints/{ds_class_name}/{config_hash}"
 
-            save_checkpoint({
-                'epoch': epoch,
-                'step': count,
-                'state_dict': model.state_dict(),
-                'optimizer': optim.state_dict(),
-            }, filename=filename)
+                config_file_name = os.path.join(folder, f"config.json")
 
-        if avg_eval_acc > 0.995:
-            print("Accuracy threshold reached, stopping early")
+                if not os.path.exists(folder):
+                    os.mkdir(folder)
+
+                if not os.path.exists(config_file_name):
+                    with open(config_file_name, 'w') as f:
+                        json.dump(config, f, indent=2)
+
+                filename = f"{folder}/epoch_{epoch}_step_{count}_loss_{avg_eval_loss:.6f}.pth.tar"
+                print(f"Saving checkpoint {filename}")
+
+                save_checkpoint({
+                    'dataset': ds_class_name,
+                    'epoch': epoch,
+                    'step': count,
+                    'state_dict': model.state_dict(),
+                    'optimizer': optim.state_dict(),
+                }, filename=filename)
+            else:
+                score_stagnation += 1
+
+        if score_stagnation > 6:
             break
+        # if avg_eval_acc > 0.995:
+        #     print("Accuracy threshold reached, stopping early")
+        #     break
 
     random_ind = random.sample(range(len(eval_set)),12)
     random_samples = [eval_set[i] for i in random_ind]
@@ -150,17 +194,17 @@ if __name__ == "__main__":
             # we 'unsqueeze' here to simulate having a batch size of 1,
             # as the model expects some batch dimension
             input = sample['spectrogram'].unsqueeze(0).to(device)
-            label = torch.tensor(sample['index']).unsqueeze(0).to(device)
+            label = torch.tensor(sample['label']).unsqueeze(0).to(device)
             output = model(input)
 
             # return the index of the maximum value of the output
             pred_idx = output.argmax(dim=1)
-            pred_label = dataset.categories[pred_idx.item()]
+            pred_label = eval_set.categories[pred_idx.item()]
 
         # Add subplot
         row, col = (i // 4) + 1, (i % 4) + 1
         fig.add_trace(go.Heatmap(z=data.cpu().squeeze(0).numpy(), colorscale='Viridis'), row=row, col=col)
-        fig.layout.annotations[i].update(text=f'Pred: {pred_label}, Label: {true_label}')
+        fig.layout.annotations[i].update(text=f'Pred: {pred_label},<br/>Label: {true_label}')
 
     fig.update_layout(
         margin=dict(l=10, r=10, t=30, b=10)
